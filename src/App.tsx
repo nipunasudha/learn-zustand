cf23871403349676fd4e148743526e4d27596e79
ParagonTheDev <nipuna.sudha@gmail.com>
2023-09-25 18:14:25 +0530

    feat: improve store

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import {useBearStore} from "./store/bear-store.ts";
 
 function App() {
-    const bears = useBearStore(store => store.bears);
-    const increase = useBearStore(store => store.increase);
+    const bears = useBearStore(state => state.bears);
+    const {
+        increase,
+        reset
+    } = useBearStore();
     return (
         <>
             <div>We have {bears} bears! 🐻</div>
             <button onClick={() => increase(1)}>Click Me!</button>
+            <button onClick={reset}>Reset!</button>
         </>
     )
 }
--- src/store/bear-store.ts
+++ src/store/bear-store.ts
@@ -1,18 +1,26 @@
-import { create } from 'zustand'
-import { devtools, persist } from 'zustand/middleware'
+import {create} from 'zustand'
+import {devtools, persist} from 'zustand/middleware'
 
 interface BearState {
     bears: number
     increase: (by: number) => void
+    reset: () => void
 }
 
 export const useBearStore = create<BearState>()(
     devtools(
         persist(
-            (set) => ({
-                bears: 0,
-                increase: (by) => set((state) => ({ bears: state.bears + by })),
-            }),
+            (set) => {
+                return {
+                    bears: 0,
+                    increase: () => {
+                        set((prevState)=>({bears: prevState.bears + 50}))
+                    },
+                    reset: () => {
+                        set({bears: 0})
+                    }
+                };
+            },
             {
                 name: 'bear-storage',
             }
