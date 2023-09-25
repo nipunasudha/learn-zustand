368109d8b0fc1ffb800beafce43d6ec058ee0cf4
ParagonTheDev <nipuna.sudha@gmail.com>
2023-09-25 17:37:01 +0530

    feat: add bear store

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import {useBearStore} from "./store/bear-store.ts";
+
 function App() {
+    const bears = useBearStore(store => store.bears);
+    const increase = useBearStore(store => store.increase);
     return (
         <>
-            <div>Something Here</div>
-            <button>Click Me!</button>
+            <div>We have {bears} bears! 🐻</div>
+            <button onClick={() => increase(1)}>Click Me!</button>
         </>
     )
 }
